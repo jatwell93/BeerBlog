a1a7772b5be19694ce105178d533a5daeb321a09
Jordan Hudgens <jordan@devcamp.com>
2017-03-18 01:38:23 +0000

    used resource generator

---
+++ db/migrate/20170318013444_create_calenders.rb
@@ -0,0 +1,13 @@
+class CreateCalenders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :calenders do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
