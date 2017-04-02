c4cf4359fd315fc3dc818ee4b0bb3bfa3c103ef3
Jordan Hudgens <jordan@devcamp.com>
2017-04-02 23:57:33 +0000

    implemented portfolio feature

--- app/controllers/calenders_controller.rb
+++ app/controllers/calenders_controller.rb
@@ -1,2 +1,54 @@
 class CalendersController < ApplicationController
+  def index
+    @calender_items = Calender.all
+  end
+  
+  def new
+    @calender_item = Calender.new
+  end
+  
+  def create
+    @calender_item = Calender.new(calender_params)
+
+    respond_to do |format|
+      if @calender_item.save
+        format.html { redirect_to calenders_path, notice: 'Calender item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
+  def edit
+    @calender_item = Calender.find(params[:id])
+  end
+  
+  def update
+    @calender_item = Calender.find(params[:id])
+    respond_to do |format|
+      if @calender_item.update(calender_params)
+        format.html { redirect_to calenders_path, notice: 'Calender was successfully updated.' }
+            else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  def show
+    @calender_item = Calender.find(params[:id])
+  end
+  
+  def destroy 
+    @calender_item = Calender.find(params[:id])
+    @calender_item.destroy
+    respond_to do |format|
+      format.html { redirect_to calenders_url, notice: 'Calender item was successfully destroyed.' }
+    end
+  end
+  
+  private 
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def calender_params
+      params.require(:calender).permit(:title, :subtitle, :body)
+    end  
 end
